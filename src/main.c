9b8f855d8d5f526bbc492a64c175fe81b6e9e8d4
YannBENOIST <yann.benoist@etu.estia.fr>
2025-06-18 15:14:10 +0200

    Second line Yann

--- src/main.c
+++ src/main.c
@@ -2,20 +2,25 @@
 #include <stdlib.h>
 #include <string.h>
 
+
 #include <estia-image.h>
 #include <getopt.h>
 
 #include "features.h"
+#include <string.h>
+
 #include "utils.h"
 #include "argsparse.h"
 
+
+  //SECOND LINE
 int main(int argc, char **argv) {
-  /*To use debug mode: 
+  /*To use debug mode:
    - add --debug: freud.exe --debug -f images/input/image.jpeg
-   or 
+   or
    - Simply run your compiled project with start button on the blue menu bar at the bottom of the VS Code window.
    */
-
+ 
   /*DO NOT EDIT THIS PART*/
   Config configuration ;
   parse_arguments( argc, argv, &configuration ) ;
@@ -23,19 +28,15 @@ int main(int argc, char **argv) {
   check_file();
   /* END */
 
-  /* Use "if ( strncmp( command, <commandname>, 9 ) == 0 )" to check if your <commandname> is called by program.*/
-  /* Example with helloworld command
-   * If helloworld is a called command: freud.exe -f images/input/image.jpeg -c helloworld 
-   */
-  if ( strncmp( configuration.command, "helloworld", 10 ) == 0 ) {
-    /* helloworld() function is defined in feature.h and implemented in feature.c */
-    helloWorld();
-  }
+if ( strncmp( configuration.command, "second_line", 11 ) == 0 ) {
+  second_line(configuration.filenames[0]); 
+}
+
   /*
    * TO COMPLETE
    */
-  
-  return 0;
+
+return 0;
 
   if ( strncmp( configuration.command, "first_pixel", 11 ) == 0 ) {
     /* first_pixel() function is defined in feature.h and implemented in feature.c */
